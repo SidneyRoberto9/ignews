a795fa281729c0f26aa6d1551988a947deda7198
Sidney Roberto <sidneyrpsilva@gmail.com>
2022-03-01 13:07:03 -0300

    ✨ feat: Consume and show registred posts

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,45 +1,44 @@
 import Head from "next/head";
 import { GetStaticProps } from "next";
+import { RichText } from "prismic-dom";
 import styles from "./styles.module.scss";
 import * as Prismic from "@prismicio/client";
 import { createClient } from "../../services/prismic";
 
-export default function Posts() {
+type Post = {
+  slug: string;
+  title: string;
+  excerpt: string;
+  updatedAt: string;
+};
+
+interface PostsProps {
+  posts: Post[];
+}
+
+export default function Posts({ posts }: PostsProps) {
   return (
     <>
       <Head>
-        <title>Posts | ig.news</title>
+        <title>Posts | Ignews</title>
       </Head>
 
       <main className={styles.container}>
         <div className={styles.posts}>
-          <a href="">
-            <time>12 de março de 2021 </time>
-            <strong>Creating a Monorepo with Lerna & yarn Workspaces</strong>
-            <p>aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</p>
-          </a>
-          <a href="">
-            <time>12 de março de 2021 </time>
-            <strong>Creating a Monorepo with Lerna & yarn Workspaces</strong>
-            <p>aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</p>
-          </a>
-          <a href="">
-            <time>12 de março de 2021 </time>
-            <strong>Creating a Monorepo with Lerna & yarn Workspaces</strong>
-            <p>aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</p>
-          </a>
-          <a href="">
-            <time>12 de março de 2021 </time>
-            <strong>Creating a Monorepo with Lerna & yarn Workspaces</strong>
-            <p>aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</p>
-          </a>
+          {posts.map((post) => (
+            <a key={post.slug} href="#">
+              <time>{post.updatedAt}</time>
+              <strong>{post.title}</strong>
+              <p>{post.excerpt}</p>
+            </a>
+          ))}
         </div>
       </main>
     </>
   );
 }
 
-export const getStaticProps: GetStaticProps = async () => {
+export const getStaticProps: GetStaticProps = async (context: any) => {
   let prismic = createClient();
 
   const response = await prismic.query(
@@ -50,29 +49,27 @@ export const getStaticProps: GetStaticProps = async () => {
     }
   );
 
-  console.log(response);
-
-  //   const publication = response.results.map((publi) => {
-  //     return {
-  //       slug: publi.uid,
-  //       title: RichText.asText(publi.data.title),
-  //       excerpt:
-  //       publi.data.content.find((content) => content.type === "paragraph")
-  //           ?.text ?? "",
-  //       updatedAt: new Date(publi.last_publication_date).toLocaleDateString(
-  //         "pt-BR",
-  //         {
-  //           day: "2-digit",
-  //           month: "long",
-  //           year: "numeric",
-  //         }
-  //       ),
-  //     };
-  //   });
+  const posts = response.results.map((post) => {
+    return {
+      slug: post.uid,
+      title: RichText.asText(post.data.title),
+      excerpt:
+        post.data.content.find((content) => content.type === "paragraph")
+          ?.text ?? "",
+      updatedAt: new Date(post.last_publication_date).toLocaleDateString(
+        "pt-BR",
+        {
+          day: "2-digit",
+          month: "long",
+          year: "numeric",
+        }
+      ),
+    };
+  });
 
   return {
     props: {
-      // publication,
+      posts,
     },
     revalidate: 60 * 5, // 5 minutes
   };
